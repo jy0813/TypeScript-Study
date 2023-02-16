c1b1b1d699952ee4a22cdfb938c2d97aab7cbaca
jy0813 <busbug3@gmail.com>
2023-02-16 16:55:43 +0900

    feat: stack 구현하기

---
+++ 3-types-oop/3-10-stack.ts
@@ -0,0 +1,46 @@
+{
+    interface Stack {
+        readonly size: number;
+        push(value: string): void;
+        pop(): string;
+    }
+
+    type StackNode = {
+        readonly value:string;
+        readonly next?:StackNode;
+    }
+
+    class StackImpl implements Stack {
+        private _size:number = 0;
+        private head?: StackNode;
+
+        constructor(private capacity:number) {}
+        get size() {
+            return this._size
+        }
+        push(value:string) {
+            if(this.size === this.capacity) {
+                throw new Error('Stack is full!')
+            }
+            this.head = {value, next: this.head};
+            this._size++;
+        }
+        pop(): string {
+            if(this.head == null) {
+                throw new Error('Stack is empty!');
+            }
+            const node = this.head;
+            this.head = node.next
+            this._size--;
+            return node.value;
+        }
+    }
+
+    const stack = new StackImpl(5);
+    stack.push('1');
+    stack.push('2');
+    stack.push('3');
+    while (stack.size != 0) {
+        console.log(stack.pop());
+    }
+}
