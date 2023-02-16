a2be7220f329015a4ef181b2f1d53dc18eb35aa6
jy0813 <busbug3@gmail.com>
2023-02-16 09:58:56 +0900

    feat: 계산기 만들어보기

---
+++ 2-types-projects/calculator.ts
@@ -0,0 +1,29 @@
+/**
+ * Let's make a calculator 🧮
+ */
+
+type Operator = 'add' | 'subtract' | 'multiply' | 'divide' | 'remainder'
+
+function calculate(operator:Operator,num1:number,num2:number):number {
+    switch (operator) {
+        case "add":
+            return num1 + num2;
+        case "subtract":
+            return num1 - num2;
+        case "multiply":
+            return num1 * num2;
+        case "divide":
+            return num1 / num2;
+        case 'remainder':
+            return num1 % num2;
+        default:
+            throw Error('Error!')
+    }
+}
+
+
+console.log(calculate('add', 1, 3)); // 4
+console.log(calculate('subtract', 3, 1)); // 2
+console.log(calculate('multiply', 4, 2)); // 8
+console.log(calculate('divide', 4, 2)); // 2
+console.log(calculate('remainder', 5, 2)); // 1
